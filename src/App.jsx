5756a943c66266d51533ecce7ddfac7de8929ff7
Shakhawat Hossain Shopon <shakhawatshopon.freelance@gmail.com>
2024-09-17 02:37:16 +0600

    Spinner and route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 function App() {
+  const Home = lazy(() => import("./Module/UserClient/Screens/Home"));
+  const NotFound = lazy(() => import("./Module/UserClient/Screens/NotFound"));
+
   return (
     <>
-      <h1 className="text-blue-700 text-2xl underline">Ecomerce</h1>
+      <Router>
+        <Suspense
+          fallback={
+            <div className="w-full flex justify-center items-center h-[90vh]">
+              <ClipLoader size={100} color="#F38160" />
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </Router>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F38160",
+        secondary: "#F0B70D",
+        textColor: "#403C39",
+      },
+    },
   },
   plugins: [],
 };
